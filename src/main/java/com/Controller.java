009f428035f93d1c37f59c663245a3e86a6959fb
tjwjdgks <tjwjdgks43@gmail.com>
2021-05-24 20:19:34 +0900

    bootstrap 적용

--- src/main/java/com/Controller.java
+++ src/main/java/com/Controller.java
@@ -1,18 +1,14 @@
 package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Controller {
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Controller.class, args);
     }
---
+++ src/main/java/com/Start.java
@@ -0,0 +1,12 @@
+package com;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Start {
+    public static void main(String[] args) {
+        SpringApplication.run(Controller.class, args);
+    }
+
+}
